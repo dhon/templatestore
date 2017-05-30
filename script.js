f3284e7a3535a0e1ea392ab7b248a1a4f9bdc3f0
dhon <morgangrimes14@gmail.com>
2017-05-30 13:41:38 -0500

    removed localhost & hosted on plnkr

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-angular.module('templateStore.templates', ['ngRoute'])
+var app = angular.module('templateStore', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider){
 	$routeProvider
-	.when('/templates', {
-		templateUrl: 'templates/templates.html',
+	.when('/', {
+		templateUrl: 'templates.html',
 		controller: 'TemplatesCtrl'
 	})
-	.when('/templates/:templateId', {
-		templateUrl: 'templates/template-details.html',
+	.when('/:templateId', {
+		templateUrl: 'template-details.html',
 		controller: 'TemplateDetailsCtrl'
 	});
 }])
 
 .controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
-	$http.get('json/templates.json').success(function(data){
-		$scope.templates = data;
+  console.log("TemplatesCtrl loaded");
+	$http.get('templates.json').then(function(response){
+		$scope.templates = response.data;
 	});
 }])
 
 .controller('TemplateDetailsCtrl', ['$scope', '$http', '$routeParams', '$filter', function($scope, $http, $routeParams, $filter){
+	console.log("TemplateDetailsCtrl loaded");
 	var templateId = $routeParams.templateId;
-	$http.get('json/templates.json').success(function(data){
-		$scope.template = $filter('filter')(data, function(d){
+	$http.get('templates.json').then(function(response){
+		$scope.template = $filter('filter')(response.data, function(d){
 			return d.id == templateId;
 		})[0];
 		$scope.mainImage = $scope.template.images[0].name;
